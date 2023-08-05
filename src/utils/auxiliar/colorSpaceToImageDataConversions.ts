89254d2e4c0e11999f7a085cd0fb9306d2e231c9
RiadOliveira <riad.oliveira@hotmail.com>
2023-08-05 10:02:41 -0300

    Decomposition operations corrections

--- src/utils/auxiliar/colorSpaceToImageDataConversions.ts
+++ src/utils/auxiliar/colorSpaceToImageDataConversions.ts
@@ -1,3 +1,5 @@
+import { cmyk, hsv } from 'color-convert';
+
 const parseComponent = (component: number) => Math.round(component * 255);
 
 export const convertRgbToImageData = ([r, g, b]: number[]) => [
@@ -8,37 +10,10 @@ export const convertRgbToImageData = ([r, g, b]: number[]) => [
 
 export const convertHsbToImageData = ([h, s, b]: number[]) =>
   [
-    [h, 1, 1],
-    [0, s, 1],
-    [0, 1, b],
-  ].map(hsbToRgb);
-
-const hsbToRgb = ([h, s, b]: number[]) => {
-  const c = b * s;
-  const x = c * (1 - Math.abs(((h / 60) % 2) - 1));
-  const m = b - c;
-
-  const [red, green, blue] = (() => {
-    switch (true) {
-      case h >= 0 && h < 60:
-        return [c, x, 0];
-      case h >= 60 && h < 120:
-        return [x, c, 0];
-      case h >= 120 && h < 180:
-        return [0, c, x];
-      case h >= 180 && h < 240:
-        return [0, x, c];
-      case h >= 240 && h < 300:
-        return [x, 0, c];
-      case h >= 300 && h < 360:
-        return [c, 0, x];
-      default:
-        return [0, 0, 0];
-    }
-  })();
-
-  return [red + m, green + m, blue + m].map(parseComponent);
-};
+    [0, 0, (h / 360) * 100],
+    [0, 0, s],
+    [0, 0, b],
+  ].map(([h, s, b]) => hsv.rgb([h, s, b]));
 
 export const convertYuvToImageData = ([y, u, v]: number[]) => {
   const yComponents = [y, y, y].map(parseComponent);
@@ -52,15 +27,10 @@ export const convertYuvToImageData = ([y, u, v]: number[]) => {
   return [yComponents, uComponents, vComponents];
 };
 
-export const convertCmykToImageData = ([c, m, y, k]: number[]) => {
-  const [parsedC, parsedM, parsedY, parsedK] = [1 - c, 1 - m, 1 - y, 1 - k].map(
-    parseComponent,
-  );
-
-  return [
-    [parsedC, 255, 255],
-    [255, parsedM, 255],
-    [255, 255, parsedY],
-    [parsedK, parsedK, parsedK],
-  ];
-};
+export const convertCmykToImageData = ([c, m, y, k]: number[]) =>
+  [
+    [c, 0, 0, 0],
+    [0, m, 0, 0],
+    [0, 0, y, 0],
+    [0, 0, 0, k],
+  ].map(([c, m, y, k]) => cmyk.rgb([c, m, y, k]));
--- src/utils/auxiliar/rgbConversions.ts
+++ src/utils/auxiliar/rgbConversions.ts
@@ -1,23 +1,6 @@
-export const rgbToHsb = ([r, g, b]: number[]) => {
-  const max = Math.max(r, g, b);
-  const min = Math.min(r, g, b);
-  const delta = max - min;
+import { rgb } from 'color-convert';
 
-  let h = 0;
-  const s = delta / max;
-  const v = max / 255;
-
-  if (delta !== 0) {
-    if (max === r) h = ((g - b) / delta) % 6;
-    else if (max === g) h = (b - r) / delta + 2;
-    else h = (r - g) / delta + 4;
-
-    h *= 60;
-    if (h < 0) h += 360;
-  }
-
-  return [h, s, v];
-};
+export const rgbToHsb = ([r, g, b]: number[]) => rgb.hsv([r, g, b]);
 
 export const rgbToYuv = ([r, g, b]: number[]) => {
   const y = 0.299 * r + 0.587 * g + 0.114 * b;
@@ -27,12 +10,4 @@ export const rgbToYuv = ([r, g, b]: number[]) => {
   return [y / 255, u / 255, v / 255];
 };
 
-export const rgbToCmyk = ([r, g, b]: number[]) => {
-  const parsedMaxRgb = 1 - Math.max(r, g, b) / 255;
-
-  const c = (1 - r / 255 - parsedMaxRgb) / parsedMaxRgb;
-  const m = (1 - g / 255 - parsedMaxRgb) / parsedMaxRgb;
-  const y = (1 - b / 255 - parsedMaxRgb) / parsedMaxRgb;
-
-  return [c, m, y, parsedMaxRgb];
-};
+export const rgbToCmyk = ([r, g, b]: number[]) => rgb.cmyk([r, g, b]);
